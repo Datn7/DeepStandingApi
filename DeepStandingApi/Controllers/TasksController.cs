90a834bf17f3d77c72e027d967e663c6b57a4e58
Datn7 <datunashvili72@gmail.com>
2025-07-15 00:13:22 +0400

    tasks controller

---
+++ DeepStandingApi/Controllers/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeepStandingApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private static List<string> tasks = new() { "Learn Angular", "Build UI", "Connect API" };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetTasks() => Ok(tasks);
+
+        [HttpPost]
+        public ActionResult AddTask([FromBody] string task)
+        {
+            tasks.Add(task);
+            return Ok();
+        }
+    }
+}
