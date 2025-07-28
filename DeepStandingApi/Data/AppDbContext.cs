f08bbd834dcb5d1e35d72b3905e0661150b9dfc2
Datn7 <datunashvili72@gmail.com>
2025-07-28 12:44:59 +0400

    added dbcontext

---
+++ DeepStandingApi/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DeepStandingApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeepStandingApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
